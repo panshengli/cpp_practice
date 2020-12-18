551712ff00b1f3416f16e334b8cf14bd4c1c29ac
Pan Shengli <18604451549@163.com>
2020-12-18 14:07:51 +0800

    added documents

---
+++ src/judge.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+ auto i = 2;
+ int b = 1;
+ int c = 2;
+
+std::cout <<"i<b+c"<< (i<b+c) <<std::endl;
+std::cout <<"i<(b+c)"<< (i<(b+c)) <<std::endl;
+ return 0;
+}
